15e55a43f1e96e5b6f768dd40dd273094e0d19e2
Vithursan Thangarasa <vthangar@Vithursans-MacBook-Pro.local>
2017-12-19 01:59:43 -0500

    Fix class label 10 to 0 for SVHN dataset

--- magnet_loss/magnet_tools.py
+++ magnet_loss/magnet_tools.py
@@ -106,7 +106,8 @@ class ClusterBatchBuilder(object):
         # TODO: for SVHN fix class labels to start from 1 to 11
         for class_idx in range(self.num_classes):
             if dataset == 'svhn':
-                pdb.set_trace()
+                if class_idx == 10:
+                    class_idx = 0
                 class_mask = self.labels == class_idx+1 # Boolean mask for selecting examples
                 #pdb.set_trace()
                 class_examples = rep_data[class_mask] # Mask features based on the class mask
