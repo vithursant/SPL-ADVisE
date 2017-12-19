a47864b64e7109f7dbf3cd412ca712142d5b198f
Vithursan Thangarasa <vthangar@Vithursans-MacBook-Pro.local>
2017-12-18 22:03:18 -0500

    Add SVHN test script

---
+++ test_random_svhn.sh
@@ -0,0 +1,1 @@
+python trainer.py --data_augmentation --model resnet18 --dataset svhn --train-batch 128 --random  --learning_rate1 0.001 --checkpoint checkpoints/svhn/1-random-resnet18-lr001-b128-dataaug
