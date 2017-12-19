ac489bd2f7091acb09b3274020a0a476bebe422e
Vithursan Thangarasa <vthangar@Vithursans-MacBook-Pro.local>
2017-12-18 22:07:48 -0500

    Add SVHN test script for SPLD

---
+++ test_spld_svhn.sh
@@ -0,0 +1,1 @@
+python trainer.py --data_augmentation --model resnet18 --dataset svhn --train-batch 128 --spld  --learning_rate1 0.001 --checkpoint checkpoints/svhn/1-spld-resnet18-lr001-b128-dataaug
