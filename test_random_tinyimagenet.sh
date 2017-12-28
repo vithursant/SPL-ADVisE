10befcb3b68c93a5e5edf95efde8fe1e4d14fedf
Vithursan Thangarasa <vthangar@Vithursans-MacBook-Pro.local>
2017-12-28 13:24:36 -0500

    Fix learning rate for Tiny-Imagenet Random test

--- test_random_tinyimagenet.sh
+++ test_random_tinyimagenet.sh
@@ -26,6 +26,6 @@
           --gamma 0.1 \
           --wd 1e-4 \
           --train-batch 256 \
-          --learning_rate1 0.001 \
+          --learning_rate1 0.1 \
           --checkpoint checkpoints/tinyimagenet/random-resnet18-bc-100-12-lr0001-b256-dataaug-iclr \
           --random
