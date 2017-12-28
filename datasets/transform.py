614f13ffd305212dfcfe4274d6b3d5f5c134af28
Vithursan Thangarasa <vthangar@Vithursans-MacBook-Pro.local>
2017-12-28 14:50:19 -0500

    Add center crop for CUB200-2011

--- datasets/transform.py
+++ datasets/transform.py
@@ -34,9 +34,9 @@ def preform_transform(args):
             #test_transform.transforms.append(transforms.CenterCrop(64))
         elif args.dataset in ['cub2002010', 'cub2002011']:
             #train_transform.transforms.append(transforms.Scale(64, padding=4))
-            train_transform.transforms.append(transforms.Resize((64, 64)))
-            train_transform.transforms.append(transforms.RandomCrop(64, padding=4))
-            test_transform.transforms.append(transforms.Resize((64, 64)))
+            train_transform.transforms.append(transforms.Resize((299, 299)))
+            train_transform.transforms.append(transforms.CenterCrop(10))
+            test_transform.transforms.append(transforms.Resize((299, 299)))
         else:
             train_transform.transforms.append(transforms.RandomCrop(32, padding=4))
 
