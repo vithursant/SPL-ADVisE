d525946c004d097356e24269a8882871fb50f540
Vithursan Thangarasa <vthangar@Vithursans-MacBook-Pro.local>
2017-12-30 13:56:07 -0500

    Add SPLD for CIFAR-100 training

--- test_wideresnet_all_cifar100.sh
+++ test_wideresnet_all_cifar100.sh
@@ -1,3 +1,20 @@
+# python  trainer.py \
+#         --dataset cifar100 \
+#         --num-classes 100 \
+#         --data_augmentation \
+#         --model wideresnet \
+#         --depth 28 \
+#         --widen-factor 10 \
+#         --dropout_rate 0.3 \
+#         --epochs 200 \
+#         --schedule 60 120 160 \
+#         --gamma 0.1 \
+#         --wd 5e-4 \
+#         --train-batch 128 \
+#         --learning_rate1 0.1 \
+#         --checkpoint checkpoints/cifar100/random-wideresnet-wf-10-d-28-lr01-b128-dataaug-iclr \
+#         --random
+
 python  trainer.py \
         --dataset cifar100 \
         --num-classes 100 \
@@ -12,5 +29,5 @@ python  trainer.py \
         --wd 5e-4 \
         --train-batch 128 \
         --learning_rate1 0.1 \
-        --checkpoint checkpoints/cifar100/random-wideresnet-wf-10-d-28-lr01-b128-dataaug-iclr \
-        --random
+        --checkpoint checkpoints/cifar100/spld-wideresnet-wf-10-d-28-lr01-b128-dataaug-iclr \
+        --spld
