d3021bfc168896477c42796d5c8b916901d6634e
Vithursan Thangarasa <vthangar@Vithursans-MacBook-Pro.local>
2018-01-01 13:10:20 -0500

    Test magnet loss cifar-100 for 500 iterations

--- optimizers/leap_selector.py
+++ optimizers/leap_selector.py
@@ -214,7 +214,7 @@ def leap_selector(  args,
     if args.dataset in ['svhn']:
         n_steps = 8000
     if args.dataset in ['cifar100']:
-        n_steps = 1000
+        n_steps = 500
 
     _ = embedding_model.train()
 
