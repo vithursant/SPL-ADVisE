1e09ce4031415387862e5da7bf80e3ed4e1088e4
Vithursan Thangarasa <vthangar@Vithursans-MacBook-Pro.local>
2018-01-01 22:30:46 -0500

    Add spld wideresnet training script

---
+++ test_spld_wideresnet.sh
@@ -0,0 +1,1 @@
+python spld_iclr.py --model wideresnet --dataset cifar100 --dropout_rate 0.3 --batch_size 128 --learning_rate1 1e-1 --spl --data_augmentation --num-classes 100 --en-scheduler --max-iter 100
