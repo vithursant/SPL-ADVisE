d832592c8b26ca0b52267ebd6c173da40b089216
Vithursan Thangarasa <vthangar@Vithursans-MacBook-Pro.local>
2018-01-03 13:05:13 -0500

    Remove random shuffle indices

--- spld_iclr.py
+++ spld_iclr.py
@@ -557,7 +557,7 @@ if args.spl:
             scheduler.step(updates)
 
         batch_train_inds = batch_builder.gen_batch_spl(spld_params[0], spld_params[1], args.batch_size)
-        np.random.shuffle(batch_train_inds)
+        #np.random.shuffle(batch_train_inds)
         train_loader.sampler.batch_indices = batch_train_inds.astype(np.int32)
 
         # Increase the learning pace
@@ -1027,7 +1027,7 @@ if args.spldml:
         # spldml_logger.writerow(row)
 
         batch_train_inds = batch_builder.gen_batch_spl(spld_params[0], spld_params[1], args.batch_size)
-        np.random.shuffle(batch_train_inds)
+        #np.random.shuffle(batch_train_inds)
         train_loader.sampler.batch_indices = batch_train_inds
 
         # Increase the learning pace
