4f7fa06653cf7b680ef91ac81e953ad464ba7846
Vithursan Thangarasa <vthangar@Vithursans-MacBook-Pro.local>
2018-11-27 23:03:49 -0800

    Remove extra comments from models.

--- models/magnet_lenet.py
+++ models/magnet_lenet.py
@@ -13,31 +13,10 @@ class MagnetLeNet(nn.Module):
 		Define the initialization function of LeNet, this function defines
 		the basic structure of the neural network
 		'''
-
-		# Inherited the parent class initialization method, that is, first
-		# run nn.Module initialization function
 		super(MagnetLeNet, self).__init__()
-		#self.embed = nn.Embedding(64, 2)
-		# Define convolution layer: input 1 channel (grayscale) picture,
-		# output 6 feature map, convolution kernel 5x5
-		self.conv1 = nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=2) # input is 28x28. padding=2 for same padding
-		# Define convolution layer: enter 6 feature maps, output 16 feature
-		# maps, convolution kernel 5x5
-		self.conv2 = nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2) # feature map size is 14*14 by pooling
-		# Define the fully connected layer: linear connection (y = Wx + b),
-		# 16 * 5 * 5 nodes connected to 120 nodes
-		#self.fc1 = nn.Linear(64*7*7, 10)
+		self.conv1 = nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=2)
+		self.conv2 = nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2) 
 		self.emb = nn.Linear(64*7*7, self.emb_dim)
-		# Define the fully connected layer: linear connection (y = Wx + b),
-		# 120 nodes connected to 84 nodes
-		#self.fc2 = nn.Linear(120, 84)
-		# Define the fully connected layer: linear connection (y = Wx + b),
-		# 84 nodes connected to 10 nodes
-		#self.fc3 = nn.Linear(84, 10)
-
-		#self.embedding = nn.Linear(2, self.emb_dim)
-
-		#self.norm_emb =  L2_Normalize(self.emb)
 
 		self.layer1 = None
 		self.layer2 = None
@@ -50,53 +29,16 @@ class MagnetLeNet(nn.Module):
 		Define the forward propagation function and automatically generates
 		the backward propagation function (autograd)
 		'''
-
-		# Input x -> conv1 -> relu -> 2x2 the largest pool of windows ->
-		# update to x
 		x = F.max_pool2d(F.relu(self.conv1(x)), 2)
 		self.layer1 = x
-
-		# Input x -> conv2 -> relu -> 2x2 window maximum pooling -> update
-		# to x
 		x = F.max_pool2d(F.relu(self.conv2(x)), 2)
 		self.layer2 = x
-
-		# The view function changes the tens x into a one-dimensional vector
-		# form with the total number of features unchanged, ready for the
-		# fully connected layer
 		x = x.view(-1, self.num_flat_features(x))
-		#x = x.view(-1, self.num_flat_features(x))
 		self.features = x
-		#self.features = features
-		#features.register_hook(print)
-
-		# Input x -> fc1 -> relu, update to x
-		#x = F.relu(self.fc1(x))
 		out1 = self.emb(x)
-		#fc1 = x
 		embeddings = out1
-
-		# out2 = self.fc1(x)
-		# prediction = F.softmax(out2)
-		#pdb.set_trace()
-		# Input x -> fc2 -> relu, update to x
-		#x = F.relu(self.fc2(x))
-		#fc2 = x
-
-		# Input x -> fc3 -> relu, update to x
-		#x = self.fc3(x)
-		#fc3 = x
-
-		#pdb.set_trace()
-
-		#x = self.embedding(x)
-		#pdb.set_trace()
-		#x.register_hook(print)
-		#x = self.norm_emb(x)
-
-		#norm_emb = x
 		norm_embeddings = self.l2_normalize(out1, 1)
-		#pdb.set_trace()
+
 		return embeddings, norm_embeddings
 
 	def num_flat_features(self, x):
